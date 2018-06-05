66ae889921103dc501d53a68ab055926ea3532e1
Jennifer Stoos <jstoos@bitlaw.com>
2018-06-05 20:39:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,5 +27,11 @@ class App < Sinatra::Base
     erb :say_words
   end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+  end
+  
 
 end
